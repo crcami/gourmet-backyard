1be5e351ac09f1ce44c01bafe9799186890209a3
Camila Ramos <camilaafonsolemes@gmail.com>
2024-06-18 15:14:54 -0300

    Feat: Introducing a new component called Testimonial.

---
+++ src/Components/Testimonial.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ProfilePic from "../Assets/john-doe-image.png"
+import { AiFillStar } from 'react-icons/ai'
+
+const Testimonial = () => {
+    return (
+        <div className="work-section-wrapper">
+            <div className="work-section-top">
+                <p className="primary-subheading"> Testimonial</p>
+                <h1 className="primary-heading">
+                    What they Are Saying
+                </h1>
+                <p classNmae="primary-text">
+                    Here are some comments about Gourmet Backyard. Customers love the combination of fresh and delicious
+                    dishes with the convenience of delivery. You can choose how often you'dlike
+                    to receive these gourmet meals, enjoying a unique dining experience at home.
+                </p>
+            </div>
+            <div className="testimonial-section-botton">
+                <img src={ProfilePic} alt="" />
+                <p>
+                    Gourmet Backyard is a charming outdoor restaurant offering fresh and delicious dishes prepared
+                    with garden-fresh ingredients. With flexible delivery options, you can enjoy this unique dining
+                    experience as often as you like, right in the comfort of your home. The impeccable service and
+                    tranquil ambiance make it perfect for any occasion.
+                </p>
+                <div className="testimonial-stars-container">
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                </div>
+                <h2>John Doe</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonial
